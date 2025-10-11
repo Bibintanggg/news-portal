f111468d858d861ac3bfa7e04bf2f7fc482abbf0
Bibintanggg <bintangyudhaa8@gmail.com>
2025-10-11 17:42:20 +0700

    feat: added setup on command with cobra

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,2 +1,15 @@
 package app
 
+import (
+	"bwa-news/config"
+
+	"github.com/rs/zerolog/log"
+)
+
+func RunServer() {
+	cfg := config.NewConfig()
+	_, err := cfg.ConnectionPostgres()
+	if err != nil {
+		log.Fatal().Msgf("Error to connect to database: %v", err)
+	}
+}
