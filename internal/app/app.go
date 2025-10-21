2ba1b57b928db4bb02328600b3bf642d8a215ae6
Bibintanggg <bintangyudhaa8@gmail.com>
2025-10-21 22:40:07 +0700

    feat: setting up fiber v2 on app.go

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,16 +3,27 @@ package app
 import (
 	"bwa-news/config"
 	"bwa-news/lib/auth"
+	"bwa-news/lib/middleware"
+	"bwa-news/lib/pagination"
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/service/s3"
-	"github.com/rs/zerolog/log"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 func RunServer() {
 	cfg := config.NewConfig()
 	_, err := cfg.ConnectionPostgres()
 	if err != nil {
-		log.Fatal().Msgf("Error to connect to database: %v", err)
+		log.Fatal("Error to connect to database: %v", err)
 	}
 
 	//Cloudflare R2
@@ -20,4 +31,39 @@ func RunServer() {
 	_ = s3.NewFromConfig(cdfR2)
 
 	_ = auth.NewJwt(cfg)
+	_ = middleware.NewMiddleware(cfg)
+	_ = pagination.NewPagination()
+
+	app := fiber.New()
+	app.Use(cors.New())
+	app.Use(recover.New())
+	app.Use(logger.New(logger.Config{
+		Format: "[${time}] %{ip} %{status} - %{latency} %{method} %{path}\n",
+	}))
+
+	_ = app.Group("/api")
+
+	go func() {
+		if cfg.App.AppPort == "" {
+			cfg.App.AppPort = os.Getenv("APP_PORT")
+		}
+
+		err := app.Listen(":" + cfg.App.AppPort)
+		if err != nil {
+			log.Fatal("Error starting server: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	signal.Notify(quit, syscall.SIGTERM)
+
+	<-quit
+
+	log.Println("server shutdown of 5 seconds")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	app.ShutdownWithContext(ctx)
+
 }
